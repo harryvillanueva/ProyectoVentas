645aff9598c9adaba23770a487ddfaad8972f732
Harry Villanueva <harryvillanueva0814@gmail.com>
2025-02-25 20:03:36 +0100

    commit para agregrar metodos a clase output

--- src/output/Output.java
+++ src/output/Output.java
@@ -12,4 +12,25 @@ public class Output {
                 5. Salir
                 """);
     }
+
+    public void mostrarMensajeReferencia(){
+        System.out.println("Ingrese la referencia");
+    }
+    public void mostrarMensajeUnidades(){
+        System.out.println("Ingrese la cantidad de unidades");
+    }
+    public void mostrarMensajefechaAlta(){
+        System.out.println("Ingrese la fecha de alta  YYYYMMDDTHH:mm");
+    }
+
+    public void mostrarMensajefechaModificacion(){
+        System.out.println("Ingrese la fecha de modificacion  YYYYMMDDTHH:mm");
+    }
+
+    public void mostrarMensajeComprar(){
+        System.out.println("Ingrese la cantidad a comprar");
+    }
+    public void mostrarMensajeVender(){
+        System.out.println("Ingrese la cantidad a vender");
+    }
 }
